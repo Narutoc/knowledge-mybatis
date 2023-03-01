27662fc44c02e0c3e3b45ac1c51a2f8fbc9b8b0b
naruto_chen <1187340828@qq.com>
2023-03-01 19:30:21 +0800

    task#002 remove unused dependency

--- src/main/java/com/local/naruto/knowledge/KnowledgeApplication.java
+++ src/main/java/com/local/naruto/knowledge/KnowledgeApplication.java
@@ -4,14 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.local.naruto.knowledge.mapper")
 @SpringBootApplication
 @EnableCaching
 @EnableTransactionManagement
-@EnableFeignClients
 public class KnowledgeApplication {
 
     public static void main(String[] args) {
